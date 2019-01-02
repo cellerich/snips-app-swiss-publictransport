8efb14c814ecf2b8a64b501d5c68dadfd9451118
Marcel Spring <marcel.spring@advis.ch>
2019-01-02 16:54:19 +0100

    updated app code

--- action-app-swiss-publictransport.py
+++ action-app-swiss-publictransport.py
@@ -4,7 +4,8 @@
 from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
-# from tesla_api import TeslaApiClient
+
+import swiss_transport_info as STI
 
 import io
 import logging
@@ -20,103 +21,131 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
+
 class Swiss_Publictransport_app(object):
     """Class used to wrap action code with mqtt connection
         
-        Swiss Publictransport app
-        Dispatch the intents to the corresponding actions
+    Swiss Publictransport app
+    Dispatch the intents to the corresponding actions
     """
 
-    transport = ''
-    origin = ''
-    destinantion = ''
+    # transport = ""
+    # origin = ""
+    # destinantion = ""
 
     def __init__(self):
+        """Initialize our app 
+        - read the config file
+        - initialize our API and Multilanguage Text handler class with 
+          correct language 
+        """
         # get the configuration if needed
         try:
             self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
-            _LOGGER.debug('reading the config file {}', self.config)
-            _LOGGER.debug('MQTT address is {}', MQTT_ADDR)
-        except :
+            _LOGGER.debug("reading the config file {}", self.config)
+            _LOGGER.debug("MQTT address is {}", MQTT_ADDR)
+        except:
             self.config = None
-            _LOGGER.error('not able to read config file!')
+            _LOGGER.error("not able to read config file!")
+
+        # get the API and Multilanguage Text handler class
+        try:
+            self.sti = STI.SwissTransportInfo(self.config["language"])
+        except Exception as e:
+            _LOGGER.error(e)
 
         # start listening to MQTT
         self.start_blocking()
 
-    def parse_slots(self,intent_message):
+    def _parse_slots(self, intent_message):
+        """Parse the received slots into class variables
+        """
 
-        # Parse the query slots, and fetch the weather forecast from Open Weather Map's API
-
+        # default origin is our home station from config
+        self.origin = self.config["home_station"]
+        # Parse the query slots
         for (slot_value, slot) in intent_message.slots.items():
-            if slot_value == 'transport_type':
-                self.transport = slot[0].slot_value.value.value.encode('utf8')
-            if slot_value == 'from_station':
-                self.origin = slot[0].slot_value.value.value.encode('utf8')
-            if slot_value == 'to_station':
-                self.destinantion = slot[0].slot_value.value.value.encode('utf8')
+            if slot_value == "transport_type":
+                self.transport = slot[0].slot_value.value.value.encode("utf8")
+            if slot_value == "from_station":
+                self.origin = slot[0].slot_value.value.value.encode("utf8")
+            if slot_value == "to_station":
+                self.destinantion = slot[0].slot_value.value.value.encode("utf8")
 
-
-
+    # -------------------------------------------------------------------------
     # --> Sub callback function, one per intent
-    #===train_schedule_to intent action ==========================================
+    # -------------------------------------------------------------------------
+
+    # ===train_schedule_to intent action ======================================
     def train_schedule_to(self, hermes, intent_message):
+        """fulfill the intent 
+        """
+
+        # log what we got
+        _LOGGER.debug("[Received] {}", intent)
+
+        # get the slots from intent
+        self._parse_slots(intent_message)
+        # call our API
+        text_to_speak = self.sti.get_connection(self.origin, self.destinantion)
         # terminate the session first if not continue
-        self.parse_slots(intent_message)
-        intent = 'Absicht erhalten: {} nach {}'.format(self.transport, self.destinantion)
-        hermes.publish_end_session(intent_message.session_id, intent.decode('utf8'))
-        
-        # action code goes here...
-        _LOGGER.debug('[Received] {}', intent)
+        hermes.publish_end_session(
+            intent_message.session_id, text_to_speak.decode("utf8")
+        )
 
-        # if need to speak the execution result by tts
-        #hermes.publish_start_session_notification(intent_message.site_id, "Noch eine Schlussbemerkung - eins", "")
-
-
-    #===train_schedule_from_to intent action ==========================================
+    # ===train_schedule_from_to intent action =================================
     def train_schedule_from_to(self, hermes, intent_message):
+        """fulfill the intent 
+        """
+
+        # log what we got
+        _LOGGER.debug("[Received] {}", intent)
+
+        # get the slots from intent
+        self._parse_slots(intent_message)
+        # call our API
+        text_to_speak = self.sti.get_connection(self.origin, self.destinantion)
         # terminate the session first if not continue
-        self.parse_slots(intent_message)
-        intent = 'Absicht erhalten: {} von {} nach {}'.format(self.transport, self.origin, self.destinantion)
-        hermes.publish_end_session(intent_message.session_id, intent.decode('utf8'))
-        
-        # action code goes here...
-        _LOGGER.debug('[Received] {}', intent)
+        hermes.publish_end_session(
+            intent_message.session_id, text_to_speak.decode("utf8")
+        )
 
-        # if need to speak the execution result by tts
-        #hermes.publish_start_session_notification(intent_message.site_id, "Noch eine Schlussbemerkung - eins", "")
-
-
-    #===station_timetable intent action ==========================================
+    # ===station_timetable intent action ======================================
     def station_timetable(self, hermes, intent_message):
+        """fulfill the intent 
+        """
+
+        # log what we got
+        _LOGGER.debug("[Received] {}", intent)
+
+        # get the slots from intent
+        self._parse_slots(intent_message)
+        # call our API
+        text_to_speak = self.sti.get_station_board(self.origin)
         # terminate the session first if not continue
-        self.parse_slots(intent_message)
-        intent = 'Absicht erhalten: {} '.format(self.transport)
-        hermes.publish_end_session(intent_message.session_id, intent.decode('utf8'))
-        
-        # action code goes here...
-        _LOGGER.debug('[Received] {}', intent)
+        hermes.publish_end_session(
+            intent_message.session_id, text_to_speak.decode("utf8")
+        )
 
-        # if need to speak the execution result by tts
-        #hermes.publish_start_session_notification(intent_message.site_id, "Noch eine Schlussbemerkung - eins", "")
-
-
+    # -------------------------------------------------------------------------
     # --> Master callback function, triggered everytime an intent is recognized
-    def master_intent_callback(self,hermes, intent_message):
-        coming_intent = intent_message.intent.intent_name
-        _LOGGER.debug('Intent receive: {}',coming_intent)
-        if coming_intent == 'cellerich:train_schedule_to':
-            self.train_schedule_to(hermes, intent_message)
-        if coming_intent == 'cellerich:train_schedule_from_to':
-            self.train_schedule_from_to(hermes, intent_message)
-        if coming_intent == 'cellerich:station_timetable':
-            self.station_timetable(hermes, intent_message)
+    # -------------------------------------------------------------------------
 
+    def master_intent_callback(self, hermes, intent_message):
+        coming_intent = intent_message.intent.intent_name
+        _LOGGER.debug("Intent received: {}", coming_intent)
+        if coming_intent == "cellerich:train_schedule_to":
+            self.train_schedule_to(hermes, intent_message)
+        if coming_intent == "cellerich:train_schedule_from_to":
+            self.train_schedule_from_to(hermes, intent_message)
+        if coming_intent == "cellerich:station_timetable":
+            self.station_timetable(hermes, intent_message)
 
     # --> Register callback function and start MQTT
     def start_blocking(self):
         with Hermes(MQTT_ADDR) as h:
             h.subscribe_intents(self.master_intent_callback).start()
+
 
 if __name__ == "__main__":
     Swiss_Publictransport_app()
